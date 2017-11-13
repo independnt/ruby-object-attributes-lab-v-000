0ddefc164de88c9289793b1599f8f79dbbd9e350
Dennis Gonzalez <gonzalezdennis6@gmail.com>
2017-11-13 18:12:30 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,15 +1,11 @@
 class Dog
 
-  def initialize(name)
-    @this_dog = name
-  end
-
   def name=(dogs_name)
     @this_dog = dogs_name
   end
 
   def name
-    @this_dog
+    puts @this_dog
   end
 
 end
